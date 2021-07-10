51fa15f2c316a9ba13a1de91cc8a9b845e4ed29a
Nolo <26887899+nolosb@users.noreply.github.com>
2021-07-10 14:43:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Custom Dropdown Button
+A Discourse theme component. Adds a button with a custom dropdown menu to the header.
---
+++ javascripts/discourse/initializers/custom-dropdown-button.js
@@ -0,0 +1,79 @@
+import {withPluginApi} from "discourse/lib/plugin-api";
+import { h } from "virtual-dom";
+import { iconNode } from "discourse-common/lib/icon-library";
+import { default as composerModal } from "discourse/models/composer";
+
+export default {
+  name: "custom-dropdown-button",
+  initialize () {
+    withPluginApi ("0.8.41", (api) => {
+      api.decorateWidget("header-buttons:after", helper => {
+
+        //if (!Discourse.User.current()) return;
+
+        let container = api.container,
+            showExtraInfo = helper.attrs.minimized,
+            currentUser = api.getCurrentUser(),
+            mobileView = helper.widget.site.mobileView,
+            composerController = container.lookup("controller:composer");
+
+        const createTopic = function() {
+          const controller = container.lookup("controller:navigation/category"),
+                category = controller.get("category.id"),
+                topicCategory = container
+                  .lookup("route:topic")
+                  .get("context.category.id"),
+                categoryd = topicCategory ? topicCategory : category;
+            
+          composerController.open({
+            action: composerModal.CREATE_TOPIC,
+            //don't use current category in composer, because we open from a global level
+            //categoryId: categoryd,
+            draftKey: composerModal.DRAFT
+          });
+        };
+            
+        let menu_links_buffer = [],
+            menu_links  = settings.dropdown_items.split('|');
+
+        if (menu_links !== null) {
+          if (menu_links.length > 0) {
+            menu_links.forEach(link => {
+              const attributes = link.split(',');
+              menu_links_buffer.push (
+                h("a.btn.btn-default.btn-small.btn-icon-text", {
+                  href: attributes[2],
+                  title: attributes[1]
+                  }, 
+                  [iconNode(attributes[0]),
+                  [h("span.d-button-label",`${attributes[1]}`)]
+                  ]
+                )
+              )
+            })
+          }
+        };
+
+        menu_links_buffer.push (
+          h("a.btn.btn-default.btn-small.btn-icon-text", { onclick: createTopic }, 
+            [iconNode(settings.new_topic_icon), 
+            [h("span.d-button-label",`${settings.new_topic_title}`)]
+            ]
+          )
+        );
+            
+        let menu_buffer = 
+        //mobileView ? [h('button.widget-button.btn.btn-primary.btn-small.btn-icon-text', iconNode(settings.button_icon))] :
+          [h('button.widget-button.btn.btn-primary.btn-small.btn-icon-text', [iconNode(settings.button_icon),
+            [h('span.d-button-label',`${settings.button_title}`)]]
+          )];
+
+        menu_buffer.push(h('div.dropdown-content', menu_links_buffer));
+
+        if (!showExtraInfo) {
+          return h('div.dropdown-button', menu_buffer)
+        }
+      })
+    })
+  }
+}
